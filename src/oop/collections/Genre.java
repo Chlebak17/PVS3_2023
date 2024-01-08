5fba74e19a69c26edc6974d4755c048d8bdd9631
jakub.matas <chlebak2801@gmail.com>
2024-01-08 13:59:42 +0100

    Storage a enum Genre

--- src/oop/collections/Genre.java
+++ src/oop/collections/Genre.java
@@ -1,5 +1,8 @@
 package oop.collections;
 
 public enum Genre {
-    COMEDY,SCI_FI,DRAMA,HORROR,ACTION
+    COMEDY("Komedie"),SCI_FI("skifi"),DRAMA("drama"),HORROR("horor"),ACTION("ekšn");
+
+    Genre(String nazev) {
+    }
 }
