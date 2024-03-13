971a59bca484033ec93f09fcb930f2b888932a84
Chlebak <chlebak2801@gmail.com>
2024-03-13 16:12:19 +0100

    Edited so now it can count word count to

--- src/Tests/ReadingFile.java
+++ src/Tests/ReadingFile.java
@@ -3,7 +3,6 @@ package Tests;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 
 public class ReadingFile {
--- src/fileworks/MovieRead.java
+++ src/fileworks/MovieRead.java
@@ -7,26 +7,34 @@ import java.util.Arrays;
 public class MovieRead {
 
     static ArrayList<Movie> movies = new ArrayList<>();
+    static int wordCount = 0;
+    public static int wordCount(String name){
+        String[] arr = name.split(" ");
+        return arr.length;
+    }
+
     static void loadMovies(String filePath){
         try {
             BufferedReader br = new BufferedReader(new FileReader(filePath));
             String line;
-            while ((line  = br.readLine()) != null){
-                System.out.println(line);
+            while ((line = br.readLine()) != null){
+//                System.out.println(line);
                 movies.add(parseMovie(line));
             }
             br.close();
         }catch (IOException e){
             System.out.println("Error :  " + e.getMessage());
         }
-
-
-
     }
+
+
     static Movie parseMovie(String line){
         Movie m;
-        String[] params = line.split(";");
+        String[] params = line.split(";" + "_" + "?");
         m = new Movie(params[0],Integer.parseInt(params[1]), Double.parseDouble(params[2]));
+        int thisWord = wordCount(params[0]);
+//        System.out.println(thisWord);
+        wordCount += thisWord;
         return m;
 
     }
@@ -34,6 +42,7 @@ public class MovieRead {
 
         loadMovies("resources\\ComparableMovies.txt");
         System.out.println(movies);
+        System.out.println(wordCount);
     }
 }
 
